ac7d8a2f81e55cb3481b386943637609abf837de
balboni <brian@brianbalboni.com>
2015-07-05 15:44:58 -0500

    Completed validations p114

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 
+  validates_uniqueness_of :email
+  validates_length_of :email, :within => 5..50
+  validates_format_of :email, :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i
+  validates_confirmation_of :password
+  
+
 has_one :profile
 has_many :articles, -> {order('published_at DESC, title ASC')},
                     :dependent => :nullify
